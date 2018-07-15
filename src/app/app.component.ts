20788a00fd8fa5038897b6939955be75126c7a0b
Preston Lamb <preston.j.lamb@gmail.com>
2018-07-15 15:12:45 -0600

    Set up the demo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,107 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+	public tagMapping = { value: 'id', display: 'description' };
+	public possibleTags: any[] = [
+		{
+			id: 1,
+			description: 'Item 1',
+		},
+		{
+			id: 2,
+			description: 'Item 2',
+		},
+		{
+			id: 3,
+			description: 'Item 3',
+		},
+		{
+			id: 4,
+			description: 'Item 4',
+		},
+		{
+			id: 5,
+			description: 'Item 5',
+		},
+		{
+			id: 6,
+			description: 'Item 6',
+		},
+		{
+			id: 7,
+			description: 'Item 7',
+		},
+		{
+			id: 8,
+			description: 'Item 8',
+		},
+		{
+			id: 9,
+			description: 'Item 9',
+		},
+		{
+			id: 10,
+			description: 'Item 10',
+		},
+	];
+	public possibleTags2: any[] = [
+		{
+			id: 1,
+			description: 'Item 1',
+		},
+		{
+			id: 2,
+			description: 'Item 2',
+		},
+		{
+			id: 3,
+			description: 'Item 3',
+		},
+		{
+			id: 4,
+			description: 'Item 4',
+		},
+		{
+			id: 5,
+			description: 'Item 5',
+		},
+		{
+			id: 6,
+			description: 'Item 6',
+		},
+		{
+			id: 7,
+			description: 'Item 7',
+		},
+		{
+			id: 8,
+			description: 'Item 8',
+		},
+		{
+			id: 9,
+			description: 'Item 9',
+		},
+		{
+			id: 10,
+			description: 'Item 10',
+		},
+	];
+
+	public startingTags = [{ id: 1, description: 'Item 1' }];
+
+	public selectedTags;
+
+	addDynamicTag(newTagDisplay: string) {
+		this.possibleTags.push({ id: this.possibleTags.length + 1, description: newTagDisplay });
+		this.startingTags = [...this.selectedTags, { id: this.possibleTags.length, description: newTagDisplay }];
+	}
+
+	tagsUpdated(list: any) {
+		this.selectedTags = [...list];
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularTagSelectModule } from 'angular-tag-select';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularTagSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
